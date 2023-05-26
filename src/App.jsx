97ed3252b43ded990906085c7329652aacbb027e
BF Ibrahim <mdibrahim36194@gmail.com>
2023-05-26 13:29:11 +0600

    all files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-red-600'>hello world</h1>
+      <button class="btn btn-primary">One</button>
+      <button class="btn btn-secondary">Two</button>
+      <button class="btn btn-accent btn-outline">Three</button>
+
+    </>
+  )
+}
+
+export default App
