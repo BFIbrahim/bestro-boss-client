97ff77feb1aea3174404275e3237ff51b731cbef
BF Ibrahim <mdibrahim36194@gmail.com>
2023-05-26 18:14:18 +0600

    category slider added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner/Banner';
 import Category from './Category/Category';
 import ChefService from './chefService/ChefService';
+import PopullarMenu from './PopullarMenu/PopullarMenu';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Category></Category>
             <ChefService></ChefService>
+            <PopullarMenu></PopullarMenu>
         </div>
     );
 };
---
+++ src/Pages/Home/PopullarMenu/PopullarMenu.jsx
@@ -0,0 +1,15 @@
+import SectionTitle from "../../../Components/Section Title/SectionTitle";
+
+const PopullarMenu = () => {
+
+    return (
+        <section>
+            <SectionTitle
+                subheading={"Check it ou"}
+                heading={"FROM OUR MENU"}
+            ></SectionTitle>
+        </section>
+    );
+};
+
+export default PopullarMenu;
