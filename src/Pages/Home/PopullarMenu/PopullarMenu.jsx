c019822c304baf05f1146c1af7821c6758346562
BF Ibrahim <mdibrahim36194@gmail.com>
2023-05-27 09:16:42 +0600

    chef service added

--- src/Pages/Home/PopullarMenu/PopullarMenu.jsx
+++ src/Pages/Home/PopullarMenu/PopullarMenu.jsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../../Components/Section Title/SectionTitle";
+import MenuIteam from "../../shared/MenuIteam/MenuIteam";
 
 const PopullarMenu = () => {
+    const [menu, setMenu] = useState([])
+    useEffect(() => {
+        fetch('menu.json')
+        .then(res => res.json())
+        .then(data => {
+            const popullarIteam = data.filter(item => item.category === "popular")
+            setMenu(popullarIteam)
+        })
+    },[])
 
     return (
         <section>
@@ -8,6 +19,15 @@ const PopullarMenu = () => {
                 subheading={"Check it ou"}
                 heading={"FROM OUR MENU"}
             ></SectionTitle>
+
+            <div>
+                {
+                    menu.map(item => <MenuIteam 
+                        key={item._id}
+                        item={item}
+                    ></MenuIteam>)
+                }
+            </div>
         </section>
     );
 };
--- src/Pages/Home/chefService/ChefService.jsx
+++ src/Pages/Home/chefService/ChefService.jsx
@@ -3,8 +3,8 @@ import './ChefService.css';
 
 const ChefService = () => {
     return (
-        <div className='mt-24 mb-24 mb-20 chef-service p-24'>
-            <div className='bg-white p-10 text-center'>
+        <div className='mt-24 mb-24 mb-20 chef-service p-24 md:rounded-sm'>
+            <div className='bg-white p-10 text-center rounded-md'>
                 <h3 className='text-4xl mb-3'>BF Resturant</h3>
                 <small className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero accusamus laborum deserunt ratione dolor officiis praesentium! Deserunt magni aperiam dolor eius dolore at, nihil iusto ducimus incidunt quibusdam nemo.</small>
             </div>
