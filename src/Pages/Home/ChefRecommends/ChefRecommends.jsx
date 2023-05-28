6a4d9262d61cf95b1ad6e9ab6ea8ccf2b49c9913
BF Ibrahim <mdibrahim36194@gmail.com>
2023-05-28 15:37:11 +0600

    Home Page done

--- src/Pages/Home/ChefRecommends/ChefRecommends.jsx
+++ src/Pages/Home/ChefRecommends/ChefRecommends.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import SectionTitle from '../../../Components/Section Title/SectionTitle';
 import img from '../../../assets/home/slide1.jpg'
+import img2 from '../../../assets/home/slide2.jpg'
+import img3 from '../../../assets/home/slide3.jpg'
 
 const ChefRecommends = () => {
     return (
@@ -10,34 +12,42 @@ const ChefRecommends = () => {
                 subheading="Should Try"
             ></SectionTitle>
 
-            <div className='md:flex justify-between'>
-                <div className='md:w-72 bg-[#F3F3F3] mb-10'>
-                    <img className='w-72 h-72' src={img} alt="" />
-                    <div className='p-4 text-center'>
-                        <h3 className='text-2xl font-semibold mb-3'>Caeser Salad</h3>
+            <div className='md:flex justify-between gap-5 mb-10'>
+                <div className="card  bg-[#F3F3F3] shadow-xl text-center mb-10">
+                    <figure><img className='w-[100%] h-72' src={img} alt="Shoes" /></figure>
+                    <div className="card-body text-center">
+                        <h2 className='text-2xl font-semibold'>Caeser Salad</h2>
                         <p>Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.</p>
-                        <button style={{ borderBottom: '2px solid #BB8506' }} className='btn rounded-md border-none text-[#BB8506] uppercase bg-[#F3F3F3]'>Add to chart</button>
+                        <div className="card-actions justify-center">
+                            <button style={{ borderBottom: '2px solid black' }} className="btn bg-[#E8E8E8] border-none text-[#BB8506]">Add to chart</button>
+                        </div>
                     </div>
                 </div>
 
-                <div className='md:w-72 bg-[#F3F3F3] mb-10'>
-                    <img className='w-72 h-72' src={img} alt="" />
-                    <div className='p-4 text-center'>
-                        <h3 className='text-2xl font-semibold mb-3'>Caeser Salad</h3>
+                <div className="card  bg-[#F3F3F3] shadow-xl text-center mb-10">
+                    <figure><img className='w-[100%] h-72' src={img2} alt="Shoes" /></figure>
+                    <div className="card-body text-center">
+                        <h2 className='text-2xl font-semibold'>Pizaa</h2>
                         <p>Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.</p>
-                        <button style={{ borderBottom: '2px solid #BB8506' }} className='btn rounded-md border-none text-[#BB8506] uppercase bg-[#F3F3F3]'>Add to chart</button>
+                        <div className="card-actions justify-center">
+                            <button style={{ borderBottom: '2px solid black' }} className="btn bg-[#E8E8E8] border-none text-[#BB8506]">Add to chart</button>
+                        </div>
                     </div>
                 </div>
 
-                <div className='md:w-72 bg-[#F3F3F3] mb-10'>
-                    <img className='w-72 h-72' src={img} alt="" />
-                    <div className='p-4 text-center'>
-                        <h3 className='text-2xl font-semibold mb-3'>Caeser Salad</h3>
+                <div className="card bg-[#F3F3F3] shadow-xl text-center mb-10">
+                    <figure><img className='w-[100%] h-72' src={img3} alt="Shoes" /></figure>
+                    <div className="card-body text-center">
+                        <h2 className='text-2xl font-semibold'>Shoup</h2>
                         <p>Lettuce, Eggs, Parmesan Cheese, Chicken Breast Fillets.</p>
-                        <button style={{ borderBottom: '2px solid #BB8506' }} className='btn rounded-md border-none text-[#BB8506] uppercase bg-[#F3F3F3]'>Add to chart</button>
+                        <div className="card-actions justify-center">
+                            <button style={{ borderBottom: '2px solid black' }} className="btn bg-[#E8E8E8] border-none text-[#BB8506]">Add to chart</button>
+                        </div>
                     </div>
+
                 </div>
             </div>
+
         </section>
     );
 };
